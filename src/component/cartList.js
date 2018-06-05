f49aa9d1de38031519ee48fcdb757df57f5e7db5
xiaogu <1296706078@qq.com>
2018-06-05 23:30:05 +0800

    -mredux2

--- src/component/cartList.js
+++ src/component/cartList.js
@@ -1,12 +1,13 @@
 //购物车列表
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import actions from "../store/actions/cart";
 class CartList extends Component {
   constructor() {
     super();
   }
   render() {
-    return (<table>
+    return (<table className="table">
       <thead>
         <tr>
           <th>全选</th>
@@ -17,11 +18,19 @@ class CartList extends Component {
         </tr>
       </thead>
       <tbody>
-        {/* {this.props.carts.map((item, index) => {
-          <tr key={index}>
-            <td>{name}</td>
+        {this.props.carts.map((item, index) =>
+          (<tr key={index}>
+            <td><input type="checkbox" checked={item.checked} onChange={() => {
+              this.props.changeCheck(item.id);
+            }} /></td>
+            <td>{item.name}</td>
+            <td>{item.count}</td>
+            <td>{item.price}</td>
+            <td><button onClick={() => {
+              this.props.removeCart(item.id)
+            }}>删除</button></td>
           </tr>
-        })} */}
+          ))}
 
       </tbody>
     </table>);
@@ -31,4 +40,4 @@ export default connect((state) => {
   return {
     carts: state.cart
   }
-})(CartList);
+}, actions)(CartList);
--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -1,13 +1,23 @@
+import * as types from "../action-types";
 let initState = [
   { id: 1, checked: true, name: "apple", price: 2342, count: 1 },
-  { id: 2, checked: true, name: "sun", price: 2341, count: 2 }
+  { id: 2, checked: true, name: "yali", price: 23, count: 1 },
+  { id: 3, checked: true, name: "sun", price: 2341, count: 2 }
 ];
 function cart(state = initState, action) {
-  // switch (action.type) {
-  //   case value:
-
-  //     break;
-  // }
+  switch (action.type) {
+    // 更改选择的状态是根据id来操控的{type:"CHANGE_CHECK",id:2}
+    case types.CHANGE_CHECK:
+      return state.map(e => {
+        if (action.id === e.id) {
+          e.checked = !e.checked;
+        }
+        return e;
+      });
+    // 删除
+    case types.REMOVE_CART:
+      return state.filter(e => e.id !== action.id);
+  }
   return state;
 }
 // cart管理
