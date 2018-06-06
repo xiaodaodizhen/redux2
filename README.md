a9a0fe3da91f2de5598f958cef58d78a463633c9
xiaogu <1296706078@qq.com>
2018-06-07 07:59:01 +0800

    react2

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 - src-history1 实现购物车功能；多级组件传参功能（爷传孙）
 - src-history2 实现高阶组件功能
 - src  
+
+
+## 安装包
+- npm install immutable
--- src/react-redux.js
+++ src/react-redux.js
@@ -1,9 +1,8 @@
 // {Provider,connect}
 import React, { Component } from "react";
-let Context = React.createContext();// 创建上下文
+let Context = React.createContext();// 创建上下文,,,可以通过Context.Provider传递参数，通过Context.Consumer接收参数。
 class Provider extends Component {
     // Provider有一个属性是store
-
     render() {
         // 返回里面的所有子组件
         // <Context.Provider></Context.Provider>的简写是<Provider><Provider>，Provider是Context的属性
@@ -14,14 +13,36 @@ class Provider extends Component {
 }
 // 最后connect 返回的组件一定是Provider的子组件
 let connect = (mapStateToProps, mapDispatchToProps) => (ComponentOld) => {
-    return class Proxy extends Component {
+    return class NewProxy extends Component {
         // 可以算出一些属性，传递给ComponentOld组件
         render() {
             return <Context.Consumer>
                 {(store) => {
-                    let state = mapStateToProps(store.getState());
-                    let actions = mapDispatchToProps(store.dispatch);
-                    return <ComponentOld {...state} {...actions}></ComponentOld>;
+                    // let state = mapStateToProps(store.getState());
+                    // let actions = mapDispatchToProps(store.dispatch);
+                    // return <ComponentOld {...state} {...actions}></ComponentOld>;
+                    // 以上方法虽然能该数据，但是不能更新视图，（视图是根据state进行更改的）
+                    class High extends Component {// 此处高阶组件的作用是为了拿到this.state，然后更改视图setState()
+                        constructor() {
+                            super();
+                            this.state = mapStateToProps(store.getState());
+                        }
+                        componentDidMount() {
+                            this.unsub = store.subscribe(() => {
+                                this.setState(mapStateToProps(store.getState()));
+                            });
+                        }
+                        componentWillUnmount() {
+                            this.unsub();
+                        }
+                        render() {
+                            let actions = mapDispatchToProps(store.dispatch);
+                            return <ComponentOld {...this.state} {...actions}></ComponentOld>
+                        }
+                    }
+
+                    return <NewProxy />
+
                 }}
             </Context.Consumer>
         }
